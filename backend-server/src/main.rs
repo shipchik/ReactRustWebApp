c89c7327c146926a03020a3f63777a692552f9dc
shipchik <52711750+shipchik@users.noreply.github.com>
2023-05-03 22:35:12 +0300

    first build backend

--- backend-server/src/main.rs
+++ backend-server/src/main.rs
@@ -1,46 +1,21 @@
-use actix_web::{get,web, App, HttpServer, Responder};
-
-use serde::{Deserialize,Serialize};
-use std::sync::Mutex;
-
-mod todoList;
-
-use todoList::services;
-
-struct AppState{
-    todolist_entries:Mutex<Vec<TodolistEntry>>,
-}
-
-#[derive(Deserialize,Serialize,Clone)]
-struct TodolistEntry{
-    id:i32,
-    date:i32,
-    title:String,
-}
-
+use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/")]
-async fn index() -> String{
-    "this is smt".to_string()
+async fn index() -> impl Responder {
+    "Hello, World!"
 }
 
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
 
 #[actix_web::main]
-async fn main() ->std::io::Result<()>{
-    let app_data =web::Data::new(AppState {
-        todolist_entries: Mutex::new(vec![])
-    });
-    HttpServer::new(move || {
-        App::new()
-            .app_data(app_data.clone())
-            .service(index)  
-            .configure(services::config)
-    })
-    .bind(("127.0.0.1",8080))?
-    .run()
-    .await
-
-
-    println!("dsfdfsd")
-
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+    .service(index)
+    .service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
