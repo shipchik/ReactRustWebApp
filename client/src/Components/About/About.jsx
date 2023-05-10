3df30eafec991487499eed99f9484ac5a881564f
Diphenylamine <amine.diphenyl@gmail.com>
2023-05-10 14:26:11 +0300

    Vitalya Gay

---
+++ client/src/Components/About/About.jsx
@@ -0,0 +1,11 @@
+import style from './About.module.css';
+
+function About (){
+    return(
+        <div className={style.item}>
+            <h1>Text</h1>
+        </div>
+    );
+}
+
+export default About;
