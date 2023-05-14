feec8dd8953d5f8ab2f1f05bca050f1747c57610
Diphenylamine <amine.diphenyl@gmail.com>
2023-05-15 02:31:55 +0300

    dsa

--- client/src/Components/AboutUs/AboutUs.jsx
+++ client/src/Components/AboutUs/AboutUs.jsx
@@ -1,20 +1,23 @@
-import style from './AboutUs.modules.css';
+import style from './AboutUs.module.css';
 import AboutHeaderBacground from '../../img/AboutHeaderBacground.png';
-import AboutMainBacground from '../../img/AboutMainBacground.png';
+import AboutMainBacground1 from '../../img/AboutMainBacground1.png';
+import AboutMainBacground2 from '../../img/AboutMainBacground2.png';
+
 
 function About() {
+    console.log(style)
     return (
         <div className={style.AboutUs}>
             <header style={{
                 backgroundImage: `url(${AboutHeaderBacground})`,
                 WebkitBackgroundSize: `cover`,
-                backgroundRepeat: `no-repeat`
+                backgroundRepeat: `auto auto`
             }}>
                 <div className='container'>
-                    <div className='HeadLine'>
-                        <h1>
+                    <div className={style.HeadLine}>
+                        <h1 className={style.HeadLineText}>
                             We are your
-                            <p>motivation</p>
+                            <p className='AlarmText'>motivation</p>
                         </h1>
                     </div>
                 </div>
@@ -22,22 +25,42 @@ function About() {
 
 
             <main style={{
-                backgroundImage: `url(${AboutMainBacground}), `,
+                backgroundImage: `url(${AboutMainBacground1}) `,
                 backgroundRepeat: `no-repeat`,
-                backgroundPosition: ` right top`
+                backgroundPosition: ` right top`,
             }}>
-                <div className='container'>
 
+                <div className='container'>
                     <div className={style.AboutUsMainConent}>
-                        <section className={style.StatisticSection}>
+
+                        {/* Statistics */}
+                        <section className={style.AboutMainSection}>
+
+                            <h3 className={style.AboutMainSectionNameTag}>
+                                Statistics
+                            </h3>
+                            <p className={style.AboutMainSectionFirstParagraf}>Field<i className='AlarmText'>Fitness</i> has been helping people stay healthy for over 30 years. More than 500 people are engaged in our hall.</p>
 
                         </section>
+
+                        {/* Our culture*/}
+                        <section className={style.AboutMainSection}>
+                            <h3 className={style.AboutMainSectionNameTag}>
+                            Our culture
+                            </h3>
+                            <p className={style.AboutMainSectionThirdParagraf}>We know that creating a positive mindset is incredibly important. We believe in encouragement, support, challenge, learning and growth to be the best! Our flexible work solutions, gym discounts and training opportunities create a positive work-life balance for all of our employees.</p>
+                        </section>
+
+
+
                     </div>
                 </div>
             </main>
 
 
-            <footer></footer>
+            <footer>
+                
+            </footer>
         </div>
 
     );
