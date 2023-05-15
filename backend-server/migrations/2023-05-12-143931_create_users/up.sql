1247faf0b3b05c1d288bf2501eca053268e845e6
shipchik <52711750+shipchik@users.noreply.github.com>
2023-05-15 11:37:41 +0300

    add profile url

--- backend-server/migrations/2023-05-12-143931_create_users/up.sql
+++ backend-server/migrations/2023-05-12-143931_create_users/up.sql
@@ -2,5 +2,9 @@
 CREATE TABLE users (
     email VARCHAR NOT NULL,
     username VARCHAR NOT NULL PRIMARY KEY,
-    password VARCHAR NOT NULL
+    password VARCHAR NOT NULL,
+    telephone VARCHAR NOT NULL,
+    groups VARCHAR NOT NULL,
+    plans VARCHAR NOT NULL,
+    photo VARCHAR NOT NULL
 )
--- backend-server/src/models.rs
+++ backend-server/src/models.rs
@@ -10,20 +10,48 @@ pub struct User {
     pub email: String,
     pub username: String,
     pub password: String,
+    pub telephone: String,
+    pub groups:String,
+    pub plans:String,
+    pub photo:String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SlimUser {
     pub username: String,
+    
 }
 
 impl From<User> for SlimUser {
     fn from(user: User) -> Self {
         SlimUser {
             username: user.username,
+            
         }
     }
 }
+
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct GetMe{
+    pub username:String,
+    pub email:String,
+    pub photo:String
+    
+}
+
+impl From<User> for GetMe {
+    fn from(user: User) -> Self {
+        GetMe {
+            username: user.username,
+            email:user.email,
+            photo:user.photo
+        }
+    }
+}
+
+
+
 
 #[derive(Debug,Serialize,Deserialize)]
 pub struct LoginUser {
@@ -35,4 +63,40 @@ impl From<User> for LoginUser{
     fn from(user:User)-> Self{
         LoginUser { username: user.username, password: user.password }
     }
+}
+
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct RegisterUser{
+    pub email: String,
+    pub username: String,
+    pub password: String,
+}
+
+
+impl From<User> for RegisterUser{
+    fn from(user:User)->Self{
+        RegisterUser { email: user.email, username: user.username, password: user.password }
+    }
+}
+
+
+
+impl User{
+    pub fn reg_user(info:RegisterUser)->Self{
+        User { email: info.email, username: info.username, password: info.password, telephone:"".to_string(), groups:"".to_string(), plans:"".to_string(), photo:"".to_string() }
+    }
+    
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+
+pub struct ProfileData{
+    pub email: String,
+    pub username: String,
+    
+    pub telephone: String,
+    pub groups:String,
+    pub plans:String,
+    pub photo:String,
 }
